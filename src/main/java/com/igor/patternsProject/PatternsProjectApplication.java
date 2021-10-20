032883046c4367e24311ebee983f3dcc05772dc6
Igor <igorgf916@gmail.com>
2021-10-19 21:11:05 -0300

    Inital Commit

---
+++ src/main/java/com/igor/patternsProject/PatternsProjectApplication.java
@@ -0,0 +1,15 @@
+package com.igor.patternsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class PatternsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/patternsProject/PatternsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.patternsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
