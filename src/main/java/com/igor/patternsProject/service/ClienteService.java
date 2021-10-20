aa6c2413474a13ff5493e7c2f9b3f23ab16ac981
Igor <igorgf916@gmail.com>
2021-10-19 21:35:52 -0300

    Adicionando services

---
+++ src/main/java/com/igor/patternsProject/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.igor.patternsProject.service;
+
+import com.igor.patternsProject.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
